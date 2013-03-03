4da3a0987cf679cf98e78f079118f42bcd102749
Kaz Sasa <kazssym@vx68k.org>
2013-03-03 15:55:53 +0900

    Implemented command line options (fixes issue #5).

--- src/main.c
+++ src/main.c
@@ -22,35 +22,87 @@
 #define _GNU_SOURCE 1
 
 #include "llmnr.h"
+#include <getopt.h>
 #include <syslog.h>
 #include <unistd.h>
 #include <string.h>
+#include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct options {
+struct program_options {
     bool foreground;
 };
 
 static llmnr_responder_t responder;
 
-int main(int argc, char **argv) {
-    struct options options = {
-        .foreground = true,
+static int parse_options(int, char *[], struct program_options *restrict);
+
+int main(int argc, char *argv[argc + 1]) {
+    struct program_options options = {
+        .foreground = false,
+    };
+    if (parse_options(argc, argv, &options) >= 0) {
+        openlog(basename(argv[0]), LOG_PERROR, LOG_DAEMON);
+
+        if (llmnr_responder_create(&responder) < 0) {
+            syslog(LOG_ERR, "Could not create a responder object: %m");
+            syslog(LOG_INFO, "Exiting");
+            exit(EXIT_FAILURE);
+        }
+
+        if (options.foreground || daemon(false, false) == 0) {
+            llmnr_responder_run(responder);
+        }
+
+        llmnr_responder_delete(responder);
+    }
+
+    return 0;
+}
+
+int parse_options(int argc, char *argv[argc + 1],
+        struct program_options *restrict options) {
+    enum opt {
+        OPT_VERSION = UCHAR_MAX + 1,
+        OPT_HELP,
+    };
+    static const struct option longopts[] = {
+        {"foreground", no_argument, 0, 'f'},
+        {"help", no_argument, 0, OPT_HELP},
+        {"version", no_argument, 0, OPT_VERSION},
+        {0, 0, 0, 0},
     };
 
-    openlog(basename(argv[0]), LOG_PERROR, LOG_DAEMON);
-    
-    if (llmnr_responder_create(&responder) < 0) {
-        syslog(LOG_ERR, "Could not create a responder object: %m");
-        syslog(LOG_INFO, "Exiting");
-        exit(EXIT_FAILURE);
-    }
+    bool help = false;
+    bool version = false;
+    int opt;
+    do {
+        opt = getopt_long(argc, argv, "f", longopts, 0);
+        switch (opt) {
+        case 'f':
+            options->foreground = true;
+            break;
+        case OPT_HELP:
+            help = true;
+            break;
+        case OPT_VERSION:
+            version = true;
+            break;
+        case '?':
+            return -1;
+        }
+    } while (opt >= 0);
 
-    if (options.foreground || daemon(false, false) == 0) {
-        llmnr_responder_run(responder);
+    if (version) {
+        printf("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
+        return -1;
     }
-
-    llmnr_responder_delete(responder);    
+    if (help) {
+        // TODO: Show help.
+        fputs("No help yet.\n", stderr);
+        return -1;
+    }
     return 0;
 }

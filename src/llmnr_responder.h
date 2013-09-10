3258c8e59608f9532151984b01178511d9ad7a1c
Kaz Nishimura <kazssym@vx68k.org>
2013-09-10 18:11:38 +0900

    Updated the author name.

--- src/llmnr_responder.h
+++ src/llmnr_responder.h
@@ -1,6 +1,6 @@
 /*
  * <llmnr_responder.h> - LLMNR responder (interface)
- * Copyright (C) 2013  Kaz Sasa
+ * Copyright (C) 2013  Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free

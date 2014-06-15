65b44b43edc0e079d962e70b38f7dc0fd62b644a
Kaz Nishimura <kazssym@vx68k.org>
2014-06-15 18:26:19 +0900

    Added a new file for default dependencies for ifaddr (see issue #47).
    --HG--
    branch : feature/ifaddr-deps

---
+++ src/ifaddr_deps.c
@@ -0,0 +1,36 @@
+/*
+ * Dependencies for ifaddr (implementation)
+ * Copyright (C) 2013-2014 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+#undef _GNU_SOURCE
+
+#include "ifaddr_deps.h"
+
+#if HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif
+#if HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
+const struct ifaddr_deps ifaddr_deps_default = {
+    .close = &close,
+    .socket = &socket,
+};
---
+++ src/ifaddr_deps.h
@@ -0,0 +1,29 @@
+/*
+ * Dependencies for ifaddr (interface)
+ * Copyright (C) 2013-2014 Kaz Nishimura
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IFADDR_DEPS_H
+#define IFADDR_DEPS_H 1
+
+#include <ifaddr.h>
+
+/**
+ * Default dependencies.
+ */
+extern const struct ifaddr_deps ifaddr_deps_default;
+
+#endif

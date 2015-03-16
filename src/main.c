e1a100640aed46a9668c80118101384cbe69459a
Kaz Nishimura <kazssym@vx68k.org>
2015-03-16 23:12:25 +0900

    Removed the macro definitions for exit status.

--- src/main.c
+++ src/main.c
@@ -51,16 +51,6 @@
 #define LOG_PERROR 0
 #endif
 
-#ifndef EX_USAGE
-#define EX_USAGE 64
-#endif
-#ifndef EX_OSERR
-#define EX_OSERR 71
-#endif
-#ifndef EX_CANTCREAT
-#define EX_CANTCREAT 73
-#endif
-
 // Copyright years for printing.
 #ifndef COPYRIGHT_YEARS
 #define COPYRIGHT_YEARS "2013-2014"

e50665d576cb45912bef4f7b631e0cf03fcffa4d
Kaz Nishimura <kazssym@linuxfront.com>
2019-12-09 11:48:01 +0900

    Update copyright years

--- libxllmnrd/ifaddr.c
+++ libxllmnrd/ifaddr.c
@@ -1,6 +1,6 @@
 /*
  * ifaddr - interface addresses (implementation)
- * Copyright (C) 2013-2014 Kaz Nishimura
+ * Copyright (C) 2013-2019 Kaz Nishimura
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
